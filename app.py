cdce196ce798ded602d47ced8899453708836c74
ZoeyZhu2 <133013012+ZoeyZhu2@users.noreply.github.com>
2026-06-08 14:52:38 -0400

    added render config

--- app.py
+++ app.py
@@ -94,6 +94,8 @@ def generate():
         traceback.print_exc()
         return jsonify({"error": str(e)}), 500
 
+import os
 
 if __name__ == "__main__":
-    app.run(debug=True) #auto restart when I make changes
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
